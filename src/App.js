f8fb59e9e60b49fb976e39ea4360dc534ab012f4
Petr Zalizko <petrzalizko@Petrs-MacBook-Air.local>
2025-01-25 17:13:38 +0100

    Added simple navigation, case 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home, About, NotFound } from './components/Task1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      {/* Basic configuration 1 */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+
+      <div className='div'>
+        <p>Lorem import</p>
+      </div>
+
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/components/Task1.js
@@ -0,0 +1,11 @@
+export function Home(){
+    return <h2>Main</h2>;
+}
+
+export function About(){
+    return <h2>About</h2>;
+}
+
+export function NotFound(){
+    return <h2>Not Found 404</h2>
+}

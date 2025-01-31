ebe4d9c6010797a7f25cdd1a13a344619771e0c2
Petr Zalizko <petrzalizko@Petrs-MacBook-Air.local>
2025-01-31 10:52:05 +0100

    Added Bootstrap style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { Home, About, NotFound } from './components/Task1';
 import MainPageFromTask2 from './components/Task2';
 import MainPageFromTask3 from './components/Task3';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import VesselsNavigation from './components/VesselsNavigation.js';
 import VesselsHome from './components/VesselsHome.js';
 import VesselsAbout from './components/VesselsAbout.js';
@@ -34,7 +34,7 @@ function App() {
 
       {/* Primitive header */}
       
-      <hr></hr>
+      {/* <hr></hr> */}
 
       {/* Basic configuration 1 */}
       {/* 
@@ -70,7 +70,7 @@ function App() {
       */}
 
       {/* HW */}
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={ <VesselsNavigation/> }>
 
           <Route index element={ <VesselsHome/> }/>
@@ -83,7 +83,42 @@ function App() {
                  
           <Route path='*' element={ <VesselsNotFound/> }/>
         </Route>
-      </Routes>
+      </Routes> */}
+
+      {/* Bootstrap style */}
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar.Brand href='/'>Vessels App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <NavDropdown title="Vessels" id='basic-nav-dropdown'>
+              {vesselsData.map((vessel, index) => (
+                <NavDropdown.Item key={index} as={Link} to={`/vessel${index + 1}`}
+                >
+                  {vessel.name}
+
+                </NavDropdown.Item>
+              ))}
+
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <hr></hr>
+
+      <Routes>
+          <Route path="/" element={<VesselsHome />} />
+          <Route path="home" element={<VesselsHome />} />
+          <Route path="about" element={<VesselsAbout />} />
+          {vesselsData.map((vessel, index) => (
+            <Route key={index} path={`vessel${index + 1}`} element={<VesselsPage vessel={vessel} />} />
+          ))}
+          <Route path="*" element={<VesselsNotFound />} />
+        </Routes>
+
+
 
       
       </Container>
